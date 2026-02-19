aed3d037236759058adb394f778353782eabb654
brian-catcow-b <brianjbillman@gmail.com>
2026-02-18 20:36:13 -0500

    basic CMakeLists.txt with some headers built out for structures and a hello world in math.cpp

---
+++ core_include/steenrod_square.hpp
@@ -0,0 +1,15 @@
+#ifndef CORE_INCLUDE_STEENROD_SQUARE_HPP
+#define CORE_INCLUDE_STEENROD_SQUARE_HPP
+
+namespace ademma_core
+{
+typedef int SteenrodSquareOrder;
+//class SteenrodSquare
+//{
+//public:
+    //SteenrodSquareOrder mOrder {-1};
+//};
+}
+
+#endif // CORE_INCLUDE_STEENROD_SQUARE_HPP
+
