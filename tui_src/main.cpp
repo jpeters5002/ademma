cf7ee22101a0838ea22090a91afd3baef73fa0d5
brian-catcow-b <brianjbillman@gmail.com>
2026-02-21 23:19:09 -0500

    rm commented code

--- tui_src/main.cpp
+++ tui_src/main.cpp
@@ -40,48 +40,5 @@ int main (int argc, char** argv)
     ClassicalAdemPolynomial cap = classical_adem_math::admissify_classical_adem_monomial(user_input_cam);
     std::cout << ClassicalAdemPolynomial_ToString(cap) << std::endl;
     return 0;
-    /*
-    //
-    ademma_core::ClassicalAdemMonomial cam {};
-    cam.push_back(6);
-    cam.push_back(7);
-    ademma_core::ClassicalAdemPolynomial cap {};
-    std::cout << "running " << ademma_core::ClassicalAdemMonomial_ToString(cam) << " admissify..." << std::endl;
-    cap = ademma_core::math::admissify_classical_adem_monomial(cam);
-    std::cout << ademma_core::ClassicalAdemPolynomial_ToString(cap) << std::endl;
-    //
-    cam.clear();
-    cap.clear();
-    cam.push_back(6);
-    cam.push_back(7);
-    cam.push_back(4);
-    std::cout << "running " << ademma_core::ClassicalAdemMonomial_ToString(cam) << " admissify..." << std::endl;
-    cap = ademma_core::math::admissify_classical_adem_monomial(cam);
-    std::cout << ademma_core::ClassicalAdemPolynomial_ToString(cap) << std::endl;
-    //
-    cam.clear();
-    cap.clear();
-    cam.push_back(3);
-    cam.push_back(7);
-    cam.push_back(10);
-    cam.push_back(13);
-    std::cout << "running " << ademma_core::ClassicalAdemMonomial_ToString(cam) << " admissify..." << std::endl;
-    cap = ademma_core::math::admissify_classical_adem_monomial(cam);
-    std::cout << ademma_core::ClassicalAdemPolynomial_ToString(cap) << std::endl;
-    //
-    cam.clear();
-    cap.clear();
-    cam.push_back(6);
-    cam.push_back(19);
-    cam.push_back(14);
-    cam.push_back(10);
-    cam.push_back(13);
-    cam.push_back(14);
-    cam.push_back(22);
-    std::cout << "running " << ademma_core::ClassicalAdemMonomial_ToString(cam) << " admissify..." << std::endl;
-    cap = ademma_core::math::admissify_classical_adem_monomial(cam);
-    std::cout << ademma_core::ClassicalAdemPolynomial_ToString(cap) << std::endl;
-    return 0;
-    */
 }
 
