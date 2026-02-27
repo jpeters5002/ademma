988d4bc89ac514883b739bda511c8dca3257d3e4
Brian B <brianjbillman@gmail.com>
2026-02-27 00:54:56 -0500

    add sq1_sq2n test case for both classical and r-motivic and integrate

--- test_src/main.cpp
+++ test_src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "test_cl_prod_binary_degrees_1_to_n.hpp"
+#include "test_sq1_sq2n.hpp"
 
 #define SIZEOF_LUT(lut) (sizeof(lut) / sizeof(lut[0]))
 
@@ -14,6 +15,7 @@ struct test_function_t
 
 static test_function_t test_functions[] = {
     TEST_FUNCTION(test_cl_prod_binary_degrees_1_to_n),
+    TEST_FUNCTION(test_sq1_sq2n),
 };
 
 int main(int argc, char** argv)
