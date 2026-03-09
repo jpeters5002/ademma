8ffb8744b0f2b4aa8d87608e44ce954cf17accb0
Brian B <brianjbillman@gmail.com>
2026-03-09 19:54:30 -0400

    add cm to test_sq2tothen_tothe2nplus2

--- test_include/test_sq2tothen_tothe2nplus2.hpp
+++ test_include/test_sq2tothen_tothe2nplus2.hpp
@@ -2,7 +2,8 @@
 #define TEST_INCLUDE_TEST_2TOTHEN_TOTHE2NPLUS2
 
 // in cl, (Sq^{2^{n}})^{2n+2} = 0
-// in rm, the same is proposed as true but not proven (by this calculator it works up to n = 4 but the n = 4 case takes quite a few minutes to calculate and exponentially increases in complexity as n increases)
+// in rm, the same is proposed as true but not proven (by this calculator it works up to n = 4 but the n = 4 case takes quite a few minutes to calculate and exponentially increases in complexity as n increases; n = 3 takes a second or two in Debug builds)
+// in cm, the same is true if it's true for rm
 
 int test_sq2tothen_tothe2nplus2();
 
