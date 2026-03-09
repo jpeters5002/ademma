bd1905f41f7d21b22975063465bf2c3ca77b7922
Brian B <brianjbillman@gmail.com>
2026-03-09 19:24:37 -0400

    implement cm into test_sq2minus1_sqn

--- test_include/test_sq2nminus1_sqn.hpp
+++ test_include/test_sq2nminus1_sqn.hpp
@@ -1,7 +1,7 @@
 #ifndef TEST_INCLUDE_TEST_SQ2MINUS1_SQN_HPP
 #define TEST_INCLUDE_TEST_SQ2MINUS1_SQN_HPP
 
-// in cl, Sq^{2n-1}Sq^{n} = 0
+// in cl and cm, Sq^{2n-1}Sq^{n} = 0
 // in rm, Sq^{2n-1}Sq^{n} = 0 if n is odd; Sq^{2n-1}Sq^{n} = \rhoSq^{2n-1}Sq^{n-1} if n is even
 
 int test_sq2nminus1_sqn();
