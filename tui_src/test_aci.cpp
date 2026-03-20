b3ae2a36d09822d3379b3f77fdae1906c0b6c199
brian-catcow-b <brianjbillman@gmail.com>
2026-03-19 21:00:19 -0400

    basic manual test executable for aci

---
+++ tui_src/test_aci.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+#include "arbitrary_calculation_input.hpp"
+#include "parsing_info.hpp"
+#include "setting.hpp"
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "bad number of arguments (1 expected)" << std::endl;
+        return 1;
+    }
+    ademma_core::ParsingInfo input;
+    input.mStringToParse = argv[1];
+    ademma_core::ArbitraryCalculationInput_FromString(input, ademma_core::Setting_Type::cCLASSICAL);
+    if (input.mErrorInfo.mIsError)
+    {
+        std::cout << input.GetFullErrorString() << std::endl;
+        return 1;
+    }
+    std::cout << "parsed correctly (perhaps)" << std::endl;
+    return 0;
+}
+
